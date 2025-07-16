3c60b5fa67a109a75c244a3f5c057763e1eb6052
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 13:59:48 +0530

    Refactor Navbar component for improved styling and layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
-            <Link href={"/"}>Watch Web</Link>
+    <nav className='px-4 md:px-12 py-4 md:py-6 bg-white text-black'>
+        <div className='flex items-center justify-between'>
+            <Link href={"/"} className='hidden md:inline-block text-lg font-semibold'>Watch Web</Link>
             <div>
                 <div>{/*Search Icon*/}</div>
                 <input type="text" placeholder='Search'/>
             </div>
+
+            <Link href={"/add-product"}>
+                <button>Add Product</button>
+            </Link>
         </div>
     </nav>
   )
