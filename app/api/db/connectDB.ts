1181ab92bb18992ab5584266a01a4c501ab3f8af
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 21:51:59 +0530

    Fix connectDB function implementation to establish MongoDB connection and handle errors

--- app/api/db/connectDB.ts
+++ app/api/db/connectDB.ts
@@ -1,7 +1,11 @@
+import mongoose from "mongoose"
+
 export const connectDB = async () => {
     try {
-        
-    } catch (error) {
-        
+        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error: any) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit the process with failure
     }
 }
