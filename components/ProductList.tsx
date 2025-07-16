708def4a4be09279875552f60a963fa3cc635ad2
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-17 01:08:31 +0530

    Refactor ProductList component to fetch products from API and display dynamic data

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,19 +1,33 @@
+"use client";
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import axios from 'axios';
+
+
+interface Product{  
+  _id: string;
+  image: string;
+  name: string;
+  price: number;
+}
 
 const ProductList = () => {
-    const products = ["", "", "", ""];
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      axios.get('/api/fetch-products').then((response) => setProducts(response.data.products))
+    }, [])
   return (
     <div id='product' className='px-4 md:px-12 py-5 md:py-10 flex justify-center items-center'>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 ">
-        {products.map((product, index) => (
-          <Link href='/product/123' key={index}>
-            <Image src="/dummyImg.png" alt="Product" width={1000} height={1000} 
+        {products.map((product: Product, index) => (
+          <Link href={`/product/${product._id}`} key={index}>
+            <Image src={product.image} alt="Product" width={1000} height={1000} 
                     className='max-w-[17rem] h-72 object-cover object-center rounded-lg'/>
               <div className='mt-4 '>
-                <h2 className='font-semibold text-lg'>A perfect wrist watch</h2>
-                <p className='font-medium text-sm mt-1'>Rs. 8,300/=</p>
+                <h2 className='font-semibold text-lg'>{product.name}</h2>
+                <p className='font-medium text-sm mt-1'>Rs. {product.price}</p>
               </div>
           </Link>
         ))}
