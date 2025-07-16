7497899472310b2bd384fcddaaf37b43bc95885e
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 20:35:22 +0530

    Add Footer component and update layout to include it; expand ProductList to accommodate additional products

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 
 const ProductList = () => {
-    const products = ["", "", ""];
+    const products = ["", "", "", ""];
   return (
     <div id='product' className='px-4 md:px-12 py-5 md:py-10 flex justify-center items-center'>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 ">
