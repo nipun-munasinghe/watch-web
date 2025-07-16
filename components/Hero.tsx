c0af607352fe51041571a9f456d00a2d34c8e734
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 14:35:35 +0530

    Implement Hero component with introductory text and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='min-h-[70vh] md:min-h-[60vh] lg:min-h-[90vh] flex flex-col md:flex-row justify-center items-center bg-white px-4 md:px-12'>
+        <div className="">
+            <h1>Timeless Elegance on Your Wrist</h1>
+            <p>Discover our curated collection of premium watches, crafted for those who appreciate sophistication and precision.</p>
+        </div>
+    </div>
   )
 }
 
