e56af7e244d03c8b161012ea1681ab55b145a581
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-17 02:18:20 +0530

    Implement product fetching in SearchPage component based on URL search term

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import axios from 'axios';
 import { useSearchParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SearchPage = () => {
+    const [products, setProducts] = useState([]);
     const searchParams = useSearchParams();
 
     useEffect(() => {
         const searchTermFromUrl = searchParams.get('searchTerm');
         if (searchTermFromUrl) {
-            axios.get(`/api/search?searchTerm=${searchTermFromUrl}`).then((response) => console.log("Products: ", response.data));
+            axios
+            .get(`/api/search?searchTerm=${searchTermFromUrl}`)
+            .then((response) => setProducts(response.data.products))
+            .catch((error) => console.log("Error fetching search products:", error));
             console.log(`Searching for: ${searchTermFromUrl}`);
         } 
     }, [searchParams]);
