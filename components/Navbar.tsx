79738fc1e5c526a700d836a1c87f180f5a87bf61
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-17 02:04:03 +0530

    Implement search functionality in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,20 @@
+"use client";
 import { Search } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { ChangeEvent } from 'react'
 
 const Navbar = () => {
+
+    const router = useRouter();
+    const handleSearch = async (e: ChangeEvent<HTMLInputElement>) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', e.target.value);
+
+        const searchQuery = urlParams.toString();
+
+        router.push(`/search?${searchQuery}`); // Redirect to search results page
+    }
   return (
     <nav className='px-4 md:px-12 py-4 md:py-6 bg-white text-black'>
         <div className='flex items-center justify-between'>
@@ -11,7 +23,7 @@ const Navbar = () => {
                 <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
                     <Search className='w-4 h-4' />
                 </div>
-                <input type="text" placeholder='Search' 
+                <input type="text" placeholder='Search' onChange={handleSearch}
                     className='h-[36px] relative pl-10 border-[1px] border-black/[0.7] text-sm rounded-[8px] 
                                 w-full py-2 px-3 focus:outline-none bg-transparent'/>
             </div>
