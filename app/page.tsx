522da870cc8e54200e52fcf066ec09e695b9d2cf
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 20:31:54 +0530

    Add heading for product section in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <div className="bg-[#f8f9fa]">
       <Hero />
+
+      <h2 className="w-full text-center text-2xl md:text-4xl font-semibold py-6">All Products</h2>
       <ProductList />
     </div>
   );
