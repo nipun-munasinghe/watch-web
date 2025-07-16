d11ea5992b85b0cf00df5d3f3cdea0854df8b796
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-16 14:24:55 +0530

    Refactor layout to use Lato font and remove unused Geist font imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ["100", "300", "400", "700", "900"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.className} antialiased`}
       >
         <Navbar />
         {children}
