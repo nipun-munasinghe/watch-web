bf20c8ae5aef81bfc70a682cd0ba7e9bca62027b
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-07-17 00:00:34 +0530

    Fixed some issues

--- components/AddForm.tsx
+++ components/AddForm.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { addAction } from '@/utils/addAction'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import toast from 'react-hot-toast';
 
